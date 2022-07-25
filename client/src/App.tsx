8ce9736cd5f83faa0e4700ad7836e9b5f755c874
OriusMJC <matiasjesuscontreras12@hotmail.com>
2022-07-25 12:07:20 -0300

    Login y panel, index y app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Panel from './components/Panel';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/admin'>
+          <Route path='login' element={<Login/>}/>
+          <Route path='panel' element={<Panel/>}/>
+        </Route>
+      </Routes>
     </div>
   );
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,14 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux'
+import { store } from './redux/store';
+import { BrowserRouter as Router } from 'react-router-dom';
+import axios from 'axios';
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
